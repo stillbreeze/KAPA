6972c248d5bb1a20ae43dcd954e018b2ad81c53e
ashar7 <asharjaved7@gmail.com>
2015-08-30 06:17:30 +0530

    slicing change

--- nlp_stuff/tfidf/tfidf.py
+++ nlp_stuff/tfidf/tfidf.py
@@ -33,6 +33,6 @@ for i, blob in enumerate(bloblist):
     print("Top words in document {}".format(i + 1))
     scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
     sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    for word, score in sorted_words[-3:]:
+    for word, score in sorted_words[:5]:
         print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
 
