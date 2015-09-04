4c2b592f40f656d287099434bae25a0523d034f2
ashar7 <asharjaved7@gmail.com>
2015-09-05 04:07:12 +0530

    indf complete

--- nlp_stuff/indf/indf.py
+++ nlp_stuff/indf/indf.py
@@ -6,8 +6,12 @@ import math
 from textblob import TextBlob as tb
 
 #calculates number of documents containing a word
-def n_containing(word, bloblist):
-    return sum(1 for blob in bloblist if word in blob)
+def n_containing1(word, bloblist):
+    return sum(1 for blob in bloblist if word[0] in blob)
+
+#calculates number of documents containing a word
+def n_containing2(word, bloblist):
+    return sum(1 for i in range(0,len(bloblist)-1) if word[0] in bloblist[i] and word[1] in bloblist[i+1])
 
 #returns a list of sentences
 def make_sentences(bloblist):
@@ -15,23 +19,26 @@ def make_sentences(bloblist):
 
 #calculates average unigram document frequency for a sentence
 def df_unigram(sentence,bloblist):
-    tot=sum(n_containing(word,bloblist)/len(bloblist) for word in sentence.words)
+    tot=sum(n_containing1(word,bloblist)/len(bloblist) for word in sentence.ngrams(n=1))
     return tot/len(sentence)
 
 #calculates average bigram document frequency for a sentence
-#def df_bigram(sentence,bloblist):
-
+def df_bigram(sentence,bloblist):
+    tot=sum(n_containing2(word,bloblist)/len(bloblist) for word in sentence.ngrams(n=2))
+    return tot/len(sentence)
 
 #calculates a weighted linear combination of unigrams and bigrams
-def indf(bloblist):
-    for blob in bloblist:
+def indf(bloblist,alpha):
+    for i, blob in enumerate(bloblist):
         sentences=make_sentences(blob)
-        for sentence in sentences:
+        print ("Document {}".format(i+1))
+        for j, sentence in enumerate(sentences):
             dfug=df_unigram(sentence,bloblist)
-            print dfug
-            print "\t"
-        print "\n"
-#           dfbg=df_bigram(sentence,bloblist)
+            dfbg=df_bigram(sentence,bloblist)
+            print ("Sentence {}".format(j+1))
+            print ((alpha*dfug) + ((1-alpha)*dfbg))/len(sentence)
+        print "\n\n"
+
 
 #builds a list of documents
 def make_bloblist(bloblist):
@@ -50,4 +57,4 @@ alpha = 0.3
 bloblist = []
 make_bloblist(bloblist)
 
-indf(bloblist)
+indf(bloblist,alpha)
